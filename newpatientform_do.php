edf063c60d5c642d2b3e60db75d45589f599ad35
Brandon Mac <brandonbodie@gmail.com>
2015-08-12 11:25:58 -0500

    bodie: completed $_GET commands for 0.0 form

--- newpatientform_do.php
+++ newpatientform_do.php
@@ -1,11 +1,34 @@
-address_street=4123+jklsdaf&city=Birmingham&address_state=AL&address_zip=36608&phone_number=2513430234&email_address=fjdsakl%40dfsajl.com&gender=male&ethnicity=hispanic&race=White&language=english&citizen=us+citizen&hometypeid=1&housestatid=1&numfammember=1&numchildren=0&relationship=never+married&householdincome=4320&employment=yes&disabilty=no&veteran=yes&educationid=1&medicalinsuranceid=1&physicianid=1&cooperid=1&smokeid=1&timesmoked=1&packsmoked=0.5&submit=Submit
 $fname = $_GET['fname'];
 $lname = $_GET['lname'];
 $dob = $_GET['dob_year'] . "-" . $_GET['dob_month'] . "-" . $_GET['dob_day'];
 $address_street = $_GET['address_street'];
-$city
-$address_state
-$address
+$city = $_GET['city'];
+$state = $_GET['state'];
+$zip = $_GET['zip'];
+$phone_number = $_GET['phone_number'];
+$email_address = $_GET['email_address'];
+$gender = $_GET['gender'];
+$ethnicity = $_GET['ethnicity'];
+$race = $_GET['race'];
+$language = $_GET['language'];
+$citizen = $_GET['citizen'];
+$hometype = $_GET['hometype'];
+$housestat = $_GET['housestat'];
+$numfammember = $_GET['numfammeber'];
+$numchildren = $_GET['numchildren'];
+$relationship = $_GET['relationship'];
+$householdincome = $_GET['householdincome'];
+$employment = $_GET['employment'];
+$disability = $_GET['disability'];
+$veteran = $_GET['veteran'];
+$education = $_GET['education'];
+$medicalinsurance = $_GET['medicalinsurance'];
+$physician = $_GET['physician'];
+$cooper = $_GET['cooper'];
+$smoke = $_GET['smoke'];
+$timesmoked = $_GET['timesmoked'];
+$packsmoked = $_GET[packsmoked];
+
 
 
 
