5d69ba27a019bd0f32f6458d535b63adefbd5e7c
Brandon Mac <brandonbodie@gmail.com>
2015-08-12 10:44:12 -0500

    Brandon: adding $_GET commands

---
+++ new_patient_do.php
@@ -0,0 +1,11 @@
+address_street=4123+jklsdaf&city=Birmingham&address_state=AL&address_zip=36608&phone_number=2513430234&email_address=fjdsakl%40dfsajl.com&gender=male&ethnicity=hispanic&race=White&language=english&citizen=us+citizen&hometypeid=1&housestatid=1&numfammember=1&numchildren=0&relationship=never+married&householdincome=4320&employment=yes&disabilty=no&veteran=yes&educationid=1&medicalinsuranceid=1&physicianid=1&cooperid=1&smokeid=1&timesmoked=1&packsmoked=0.5&submit=Submit
+$fname = $_GET['fname'];
+$lname = $_GET['lname'];
+$dob = $_GET['dob_year'] . "-" . $_GET['dob_month'] . "-" . $_GET['dob_day'];
+$address_street = $_GET['address_street'];
+$city
+$address_state
+$address
+
+
+
