00885516cea25b8ff7fdb8fe625b1bea8700f519
Spacecyce <slayf@uab.edu>
2015-08-13 08:38:16 -0500

    Fixes plus indentation fixes

--- newpatientform_edit.php
+++ newpatientform_edit.php
@@ -4,141 +4,141 @@ ini_set("display_errors",1);
 
 $con = new mysqli($host, $db_user, $db_pass, $db_db);
 
-
+// Query for acquiring cities from database
 $query = "SELECT `cityid`, `city` FROM `City`;";
-        $stmt_city->$con->prepare($query);
+        $stmt_city = $con->prepare($query);
         $stmt_city->execute();
         $stmt_city->store_result();
-        $stmt_city->bind_result($cityid,$city);
+        $stmt_city->bind_result($city);
 
 $query = "SELECT `stateid`, `state` FROM `State`;";
-        $stmt_state->$con->prepare($query);
+        $stmt_state = $con->prepare($query);
         $stmt_state->execute();
         $stmt_state->store_result();
         $stmt_state->bind_result($stateid,$state);
         
 $query = "SELECT `zipid`, `zip` FROM `Zip`;";
-        $stmt_zip->$con->prepare($query);
+        $stmt_zip = $con->prepare($query);
         $stmt_zip->execute();
         $stmt_zip->store_result();
         $stmt_zip->bind_result($zipid,$zip);
 
 $query = "SELECT `ethnicityid`, `ethnicity` FROM `Ethnicity`;";
-        $stmt_ethnicity->$con->prepare($query);
+        $stmt_ethnicity = $con->prepare($query);
         $stmt_ethnicity->execute();
         $stmt_ethnicity->store_result();
         $stmt_ethnicity->bind_result($ethnicityid,$ethnicity);
 
 $query = "SELECT `genderid`, `gender` FROM `Gender`;";
-        $stmt_gender->$con->prepare($query);
+        $stmt_gender = $con->prepare($query);
         $stmt_gender->execute();
         $stmt_gender->store_result();
         $stmt_gender->bind_result($genderid,$gender);
         
 $query = "SELECT `raceid`, `race` FROM `Race`;";
-        $stmt_race->$con->prepare($query);
+        $stmt_race = $con->prepare($query);
         $stmt_race->execute();
         $stmt_race->store_result();
         $stmt_race->bind_result($raceid,$race);
         
 $query = "SELECT `languageid`, `language` FROM `PrimaryLanguage`;";
-        $stmt_language->$con->prepare($query);
+        $stmt_language = $con->prepare($query);
         $stmt_language->execute();
         $stmt_language->store_result();
         $stmt_language->bind_result($languageid,$language);
         
 $query = "SELECT `citizenid`, `citizen` FROM `CitizenStatus`;";
-        $stmt_citizen->$con->prepare($query);
+        $stmt_citizen = $con->prepare($query);
         $stmt_citizen->execute();
         $stmt_citizen->store_result();
         $stmt_citizen->bind_result($citizenid,$citizen);
         
 $query = "SELECT `cooperid`, `cooper` FROM `CooperGreen`;";
-        $stmt_cooper->$con->prepare($query);
+        $stmt_cooper = $con->prepare($query);
         $stmt_cooper->execute();
         $stmt_cooper->store_result();
         $stmt_cooper->bind_result($cooperid,$cooper);
 
 $query = "SELECT `physicianid`, `physician` FROM `PrimaryPhysician`;";
-        $stmt_physician->$con->prepare($query);
+        $stmt_physician = $con->prepare($query);
         $stmt_physician->execute();
         $stmt_physician->store_result();
         $stmt_physician->bind_result($physicianid,$physician);
         
 $query = "SELECT `educationid`, `education` FROM `EducationLevel`;";
-        $stmt_education->$con->prepare($query);
+        $stmt_education = $con->prepare($query);
         $stmt_education->execute();
         $stmt_education->store_result();
         $stmt_education->bind_result($educationid,$education);
         
 $query = "SELECT `housestatid`, `housestat` FROM `HeadofHousehold`;";
-        $stmt_housestat->$con->prepare($query);
+        $stmt_housestat = $con->prepare($query);
         $stmt_housestat->execute();
         $stmt_housestat->store_result();
         $stmt_housestat->bind_result($housestatid,$housestat);
         
 $query = "SELECT `insuranceid`, `insurance` FROM `MedicalInsurance`;";
-        $stmt_insurance->$con->prepare($query);
+        $stmt_insurance = $con->prepare($query);
         $stmt_insurance->execute();
         $stmt_insurance->store_result();
         $stmt_insurance->bind_result($insuranceid,$insurance);
         
 $query = "SELECT `disabilityid`, `disability` FROM `Disability`;";
-        $stmt_disability->$con->prepare($query);
+        $stmt_disability = $con->prepare($query);
         $stmt_disability->execute();
         $stmt_disability->store_result();
         $stmt_disability->bind_result($disabilityid,$disability);
         
 $query = "SELECT `veteranid`, `veteran` FROM `Veteran`;";
-        $stmt_veteran->$con->prepare($query);
+        $stmt_veteran = $con->prepare($query);
         $stmt_veteran->execute();
         $stmt_veteran->store_result();
         $stmt_veteran->bind_result($veteranid,$veteran);
         
 $query = "SELECT `hometypeid`, `hometype` FROM `HomeType`;";
-        $stmt_hometype->$con->prepare($query);
+        $stmt_hometype = $con->prepare($query);
         $stmt_hometype->execute();
         $stmt_hometype->store_result();
         $stmt_hometype->bind_result($hometypeid,$hometype);
         
 $query = "SELECT `foodstampid`, `foodstamp` FROM `FoodStamp`;";
-        $stmt_foodstamp->$con->prepare($query);
+        $stmt_foodstamp = $con->prepare($query);
         $stmt_foodstamp->execute();
         $stmt_foodstamp->store_result();
         $stmt_foodstamp->bind_result($foodstampid,$foodstamp);
         
 $query = "SELECT `alcoholid`, `alcohol` FROM `Alcohol`;";
-        $stmt_alcohol->$con->prepare($query);
+        $stmt_alcohol = $con->prepare($query);
         $stmt_alcohol->execute();
         $stmt_alcohol->store_result();
         $stmt_alcohol->bind_result($alcoholid,$alcohol);
         
 $query = "SELECT `relationshipid`, `relationship` FROM `RelationshipStatus`;";
-        $stmt_relationship->$con->prepare($query);
+        $stmt_relationship = $con->prepare($query);
         $stmt_relationship->execute();
         $stmt_relationship->store_result();
         $stmt_relationship->bind_result($relationshipid,$relationship);
 
 $query = "SELECT `transportid`, `transport` FROM `Transport`;";
-        $stmt_transport->$con->prepare($query);
+        $stmt_transport = $con->prepare($query);
         $stmt_transport->execute();
         $stmt_transport->store_result();
         $stmt_transport->bind_result($transportid,$transport);
 
 $query = "SELECT `visittypeid`, `visittype` FROM `VisitType`;";
-        $stmt_visittype->$con->prepare($query);
+        $stmt_visittype = $con->prepare($query);
         $stmt_visittype->execute();
         $stmt_visittype->store_result();
         $stmt_visittype->bind_result($visittypeid,$visittype);
         
 $query = "SELECT `reasonforvisitid`, `reasonforvisit` FROM `ReasonforVisit`;";
-        $stmt_reasonforvisit->$con->prepare($query);
+        $stmt_reasonforvisit = $con->prepare($query);
         $stmt_reasonforvisit->execute();
         $stmt_reasonforvisit->store_result();
         $stmt_reasonforvisit->bind_result($reasonforvisitid,$reasonforvisit);
         
 $query = "SELECT `dayofvisitid`, `dayofvisit` FROM `DayofVisit`;";
-        $stmt_dayofvisit->$con->prepare($query);
+        $stmt_dayofvisit = $con->prepare($query);
         $stmt_dayofvisit->execute();
         $stmt_dayofvisit->store_result();
         $stmt_dayofvisit->bind_result($dayofvisitid,$dayofvisit);
@@ -200,25 +200,25 @@ for ($year; $year > $year_past; $year--){
         City:
         <select name="city"/>
 <?php
-        while ($stmt_city->fetch()){        
-            echo "<option value=\"$cityid\">$city</option>";
-        }
+while ($stmt_city->fetch()){        
+    echo "          <option value=\"$cityid\">$city</option>";
+}
 ?>
         </select>
         State:
         <select name="state"/>
 <?php
-        while ($stmt_state->fetch()){        
-            echo "<option value=\"$stateid\">$state</option>";
-        }
+while ($stmt_state->fetch()){        
+    echo "<option value=\"$stateid\">$state</option>";
+}
 ?>
         </select>
         Zip:
         <select name="zip"/>
 <?php
-        while ($stmt_zip->fetch()){        
-            echo "<option value=\"$zipid\">$zip</option>";
-        }
+while ($stmt_zip->fetch()){        
+    echo "<option value=\"$zipid\">$zip</option>";
+}
 ?>
         </select>
       Phone Number:
@@ -230,41 +230,41 @@ for ($year; $year > $year_past; $year--){
       Gender:
         <select name="gender"/>
 <?php
-        while ($stmt_gender->fetch()){        
-            echo "<option value=\"$genderid\">$gender</option>";
-        }
+while ($stmt_gender->fetch()){        
+    echo "<option value=\"$genderid\">$gender</option>";
+}
 ?>
         </select>
       Ethnicity:
       <select name="ethnicity"/>
 <?php
-        while ($stmt_ethnicity->fetch()){        
-            echo "<option value=\"$ethnicityid\">$ethnicity</option>";
-        }
+while ($stmt_ethnicity->fetch()){        
+    echo "<option value=\"$ethnicityid\">$ethnicity</option>";
+}
 ?>
         </select>
       Race:
         <select name="race"/>
 <?php
-        while ($stmt_race->fetch()){        
-            echo "<option value=\"$raceid\">$race</option>";
-        }
+while ($stmt_race->fetch()){        
+    echo "<option value=\"$raceid\">$race</option>";
+}
 ?>
         </select>
       What is your primary language?:
         <select name="language"/>
 <?php
-        while ($stmt_language->fetch()){        
-            echo "<option value=\"$languageid\">$language</option>";
-        }
+while ($stmt_language->fetch()){        
+    echo "<option value=\"$languageid\">$language</option>";
+}
 ?>
         </select>
       What is your Citizenship Status?:
         <select name="citizen"/>
 <?php
-        while ($stmt_citizen->fetch()){        
-            echo "<option value=\"$citizenid\">$citizen</option>";
-        }
+while ($stmt_citizen->fetch()){        
+    echo "<option value=\"$citizenid\">$citizen</option>";
+}
 ?>
         </select>
 
@@ -272,17 +272,17 @@ for ($year; $year > $year_past; $year--){
       What type of home do you live in?
         <select name="hometype">
 <?php
-        while ($stmt_hometype->fetch()){        
-            echo "<option value=\"$hometypeid\">$hometype</option>";
-        }
+while ($stmt_hometype->fetch()){        
+    echo "<option value=\"$hometypeid\">$hometype</option>";
+}
 ?>
         </select> <br />
       Are you the head of your household?
         <select name="housestat">
 <?php
-        while ($stmt_housestat->fetch()){        
-            echo "<option value=\"$housestatid\">$housestat</option>";
-        }
+while ($stmt_housestat->fetch()){        
+    echo "<option value=\"$housestatid\">$housestat</option>";
+}
 ?>
         </select> <br />
       Number of people in your household:
@@ -306,9 +306,9 @@ $numchildrens = array("0","1","2","3","4","5","6","7","8");
       What is your relationship status?
         <select name="relationship"/>
 <?php
-        while ($stmt_relationship->fetch()){        
-            echo "<option value=\"$relationshipid\">$relationship</option>";
-        }
+while ($stmt_relationship->fetch()){        
+    echo "<option value=\"$relationshipid\">$relationship</option>";
+}
 ?>
         </select> <br />
       What is your monthly household income?
@@ -316,81 +316,81 @@ $numchildrens = array("0","1","2","3","4","5","6","7","8");
       Are you currently employed?
         <select name="employment"/>
 <?php
-        while ($stmt_employment->fetch()){        
-            echo "<option value=\"$employmentid\">$employment</option>";
-        }
+while ($stmt_employment->fetch()){        
+    echo "<option value=\"$employmentid\">$employment</option>";
+}
 ?>
         </select> <br />
       Are you on Disabilty?
         <select name="disability"/>
 <?php
-        while ($stmt_disability->fetch()){        
-            echo "<option value=\"$disabilityid\">$disability</option>";
-        }
+while ($stmt_disability->fetch()){        
+    echo "<option value=\"$disabilityid\">$disability</option>";
+}
 ?>
         </select> <br />
       Are you part of the SNAP program formerly known as Foodstamps?
         <select value="foodstamp"/>
 <?php
-        while ($stmt_foodstamp->fetch()){        
-            echo "<option value=\"$foodstampid\">$foodstamp</option>";
-        }
+while ($stmt_foodstamp->fetch()){        
+    echo "<option value=\"$foodstampid\">$foodstamp</option>";
+}
 ?>
         </select>
       Are you a United States Military Veteran?
         <select name="veteran"/>
 <?php
-        while ($stmt_veteran->fetch()){        
-            echo "<option value=\"$veteranid\">$veteran</option>";
-        }
+while ($stmt_veteran->fetch()){        
+    echo "<option value=\"$veteranid\">$veteran</option>";
+}
 ?>
         </select>
       What is your highest level of education completed?
         <select name="education">
 <?php
-        while ($stmt_education->fetch()){        
-            echo "<option value=\"$educationid\">$education</option>";
-        }
+while ($stmt_education->fetch()){        
+    echo "<option value=\"$educationid\">$education</option>";
+}
 ?>
         </select> <br />
       Do you have health insurance?
         <select name="insurance">
 <?php
-        while ($stmt_insurance->fetch()){        
-            echo "<option value=\"$insuranceid\">$insurance</option>";
-        }
+while ($stmt_insurance->fetch()){        
+    echo "<option value=\"$insuranceid\">$insurance</option>";
+}
 ?>
         </select> <br />
       Do you have a primary care physican?
         <select name="physician">
 <?php
-        while ($stmt_physician->fetch()){        
-            echo "<option value=\"$physicianid\">$physician</option>";
-        }
+while ($stmt_physician->fetch()){        
+    echo "<option value=\"$physicianid\">$physician</option>";
+}
 ?>
         </select> <br />
       Is your physician at Cooper Green?
         <select name="cooper">
 <?php
-        while ($stmt_cooper->fetch()){        
-            echo "<option value=\"$cooperid\">$cooper</option>";
-        }
+while ($stmt_cooper->fetch()){        
+    echo "<option value=\"$cooperid\">$cooper</option>";
+}
 ?>
         </select> <br />
       If current or past smoker, for how many years have you (or did you) smoke?
         <select name="timesmoked">
 <?php
-    for ($timesmoked_array = 1; $timesmoked_array < 71; $timesmoked_array++) {
-        echo "        <option value=\"$timesmoked_array\">$timesmoked_array</option>\n";
-    }
+for ($timesmoked_array = 1; $timesmoked_array < 71; $timesmoked_array++) {
+    echo "        <option value=\"$timesmoked_array\">$timesmoked_array</option>\n";
+}
 ?>
         </select>
       If current or past smoker, how many packs a day do/did you smoke?
         <select name="packsmoked">
 <?php
-    $packsmokeds = array("0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6","6.5","7");
-        for ($packsmoked_count = 0; $packsmoked_count <count($packsmokeds)+1; $packsmoked_count++){
-            echo"        <option value=\"$packsmokeds[$packsmoked_count]\">$packsmokeds[$packsmoked_count]</option>\n";
+$packsmokeds = array("0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6","6.5","7");
+    for ($packsmoked_count = 0; $packsmoked_count <count($packsmokeds)+1; $packsmoked_count++){
+        echo"        <option value=\"$packsmokeds[$packsmoked_count]\">$packsmokeds[$packsmoked_count]</option>\n";
         }
 ?>
         </select>
@@ -398,17 +398,17 @@ $numchildrens = array("0","1","2","3","4","5","6","7","8");
       How often do you drink alcohol?
         <select value="alcohol"/>
 <?php
-        while ($stmt_alcohol->fetch()){        
-            echo "<option value=\"$alcoholid\">$alcohol</option>";
-        }
+while ($stmt_alcohol->fetch()){        
+    echo "<option value=\"$alcoholid\">$alcohol</option>";
+}
 ?>
         </select>
       How do you primarily get to clinic?
         <select value="transport"/>
 <?php
-        while ($stmt_transport->fetch()){        
-            echo "<option value=\"$transportid\">$transport</option>";
-        }
+while ($stmt_transport->fetch()){        
+    echo "<option value=\"$transportid\">$transport</option>";
+}
 ?>
         </select>
        How did you hear about EAB? 
@@ -417,25 +417,25 @@ $numchildrens = array("0","1","2","3","4","5","6","7","8");
        How would you describe your visit type?
         <select value="visittype"/>
 <?php
-        while ($stmt_visittype->fetch()){        
-            echo "<option value=\"$visittypeid\">$visittype</option>";
-        }
+while ($stmt_visittype->fetch()){        
+    echo "<option value=\"$visittypeid\">$visittype</option>";
+}
 ?>
         </select>
         Which option best describes the reason for your visit?
         <select value="reasonforvisit"/>
 <?php
-        while ($stmt_reasonforvisit->fetch()){        
-            echo "<option value=\"$reasonforvisitid\">$reasonforvisit</option>";
-        }
+while ($stmt_reasonforvisit->fetch()){        
+    echo "<option value=\"$reasonforvisitid\">$reasonforvisit</option>";
+}
 ?>
         </select>
         Are you visiting on Wednesday or Sunday?
         <select value="dayofvisit"/>
 <?php
-        while ($stmt_dayofvisit->fetch()){        
-            echo "<option value=\"$dayofvisitid\">$dayofvisit</option>";
-        }
+while ($stmt_dayofvisit->fetch()){        
+    echo "<option value=\"$dayofvisitid\">$dayofvisit</option>";
+}
 ?>
         </select>
     
