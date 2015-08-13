5baf57eda47bbc08c85fb5084cfc892eac7a14c5
Brandon Mac <brandonbodie@gmail.com>
2015-08-13 10:35:26 -0500

    Variable changes

--- newpatientform_doGH.php
+++ newpatientform_doGH.php
@@ -1,36 +1,51 @@
+foodstampid = ?
+heareab=Friend&mammogram_month=1&mammogram_day=1&mammogram_year=2015&colonoscopy_month=1&colonoscopy_day=1&colonoscopy_year=2015&STI_month=1&STI_day=1&STI_year=2015&PAP_month=1&PAP_day=1&PAP_year=2015&submit=Submit
+
+<?php
+
 $fname = $_GET['fname'];
 $lname = $_GET['lname'];
 $dob = $_GET['dob_year'] . "-" . $_GET['dob_month'] . "-" . $_GET['dob_day'];
 $address_street = $_GET['address_street'];
-$city = $_GET['city'];
-$state = $_GET['state'];
-$zip = $_GET['zip'];
+$cityid = $_GET['cityid'];
+$stateid = $_GET['stateid'];
+$zipid = $_GET['zipid'];
 $phone_number = $_GET['phone_number'];
 $email_address = $_GET['email_address'];
-$gender = $_GET['gender'];
-$ethnicity = $_GET['ethnicity'];
-$race = $_GET['race'];
-$language = $_GET['language'];
-$citizen = $_GET['citizen'];
-$hometype = $_GET['hometype'];
-$housestat = $_GET['housestat'];
-$numfammember = $_GET['numfammeber'];
+$genderid = $_GET['genderid'];
+$ethnicityid = $_GET['ethnicityid'];
+$raceid = $_GET['raceid'];
+$languageid = $_GET['languageid'];
+$citizenid = $_GET['citizenid'];
+$hometypeid = $_GET['hometypeid'];
+$housestatid = $_GET['housestatid'];
+$numfammember = $_GET['numfammember'];
 $numchildren = $_GET['numchildren'];
-$relationship = $_GET['relationship'];
+$relationshipid = $_GET['relationshipid'];
 $householdincome = $_GET['householdincome'];
-$employment = $_GET['employment'];
-$disability = $_GET['disability'];
-$veteran = $_GET['veteran'];
-$education = $_GET['education'];
-$medicalinsurance = $_GET['medicalinsurance'];
-$physician = $_GET['physician'];
-$cooper = $_GET['cooper'];
-$smoke = $_GET['smoke'];
+$employmentid = $_GET['employmentid'];
+$disabilityid = $_GET['disabilityid'];
+$foodstampid = $_GET['foodstampid']
+$veteranid = $_GET['veteranid'];
+$educationid = $_GET['educationid'];
+$insuranceid = $_GET['insuranceid'];
+$physicianid = $_GET['physicianid'];
+$cooperid = $_GET['cooperid'];
 $timesmoked = $_GET['timesmoked'];
-$packsmoked = $_GET[packsmoked];
+$packsmoked = $_GET['packsmoked'];
+$alcoholid -> ???
+$transportid -> ???
 
-fname=Steve&lname=Layfield&dob_month=1&dob_day=1&dob_year=2015&address_street=1420+10th+Street+South&city=1&state=1&zip=1&phone_number=2566655142&email_address=slayf%40uab.edu&gender=1&ethnicity=1&race=1&language=1&citizen=1&hometype=1&housestat=1&numfammember=1&numchildren=0&relationship=1&householdincome=50000&employment=1&disability=1&veteran=1&education=1&insurance=1&physician=1&cooper=1&timesmoked=1&packsmoked=0.5&heareab=Friend&mammogram_month=1&mammogram_day=1&mammogram_year=2015&colonoscopy_month=1&colonoscopy_day=1&colonoscopy_year=2015&STI_month=1&STI_day=1&STI_year=2015&PAP_month=1&PAP_day=1&PAP_year=2015&submit=Submit
+$heareab
+$reasonforvisitid
+$dayofvisitid
+$mammogram___none ids
+$col
+$STI
+$Pap
+$submit?!?
 
 
+?>
 
 
